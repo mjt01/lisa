a6bf8f66530a34743f94903a7d975c67cfc90a15
mjt01 <michael.taranto@gmail.com>
2012-11-27 10:25:22 +1100

    fix for textfield requiring abstractfield from incorrect path

--- src/ui/fields/textfield.js
+++ src/ui/fields/textfield.js
@@ -1,4 +1,4 @@
-define( [ "../abstractfield/abstractfield" ], function( AbstractField ){
+define( [ "../fields/abstractfield" ], function( AbstractField ){
 	return AbstractField.extend({
 		_fieldTemplate: function() { return (
 			{ tag: "INPUT", id: this.id("field"), type: "text" }
